0042a24d109ee63fe9b210d06e430abf17f88300
R-SAI-CHARAN-TEJA <Saicharanteja.sct@gmail.com>
2026-09-13 18:35:27 +0530

    TEST 1

--- backend.py
+++ backend.py
@@ -9,14 +9,13 @@ from mysql.connector import Error
 def get_connection():
     """
     Connect Python to MySQL database.
-    Change the password to your MySQL password.
     """
 
     try:
         connection = mysql.connector.connect(
             host="localhost",
             user="root",
-            password="YOUR_MYSQL_PASSWORD",
+            password="Charan",
             database="student_db"
         )
 
